05d1691e044b26b903e11d3fc00e8f347d305de6
freddy24-7 <freddy.austli@aol.com>
2024-03-25 09:23:48 +0100

    starting set-up

---
+++ app/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '@/assets/styles/globals.css';
+
+const MainLayout = ( {children} ) => {
+    return (
+        <html lang='en'>
+            <body>
+                <div>{children}</div>
+            </body>
+        </html>
+    );
+};
+
+export default MainLayout;
