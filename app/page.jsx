2a66928455180332aa603246383c8fcd350e2ebe
freddy24-7 <freddy.austli@aol.com>
2024-03-26 15:28:23 +0100

    adding database

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import Hero from '@/components/Hero';
 import InfoBoxes from '@/components/InfoBoxes';
 import HomeProperties from "@/components/HomeProperties";
 const HomePage = () => {
+
     return (
         <>
             <Hero />
