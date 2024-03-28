420b3afe68d7913b2c668d804ac34f553b990093
freddy24-7 <freddy.austli@aol.com>
2024-03-28 12:07:54 +0100

    adding authentication

--- utils/requests.js
+++ utils/requests.js
@@ -1,14 +1,17 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
 // Fetch all properties
-async function fetchProperties() {
+async function fetchProperties({ showFeatured = false } = {}) {
     try {
         // Handle the case where the domain is not available yet
         if (!apiDomain) {
             return [];
         }
+
         const res = await fetch(
-            `${apiDomain}/properties`);
+            `${apiDomain}/properties${showFeatured ? '/featured' : ''}`,
+            { cache: 'no-store' }
+        );
 
         if (!res.ok) {
             throw new Error('Failed to fetch data');
@@ -41,7 +44,5 @@ async function fetchProperty(id) {
         return null;
     }
 }
-
-
 
 export { fetchProperties, fetchProperty };
