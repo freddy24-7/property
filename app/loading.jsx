6ef57bba3ee51ce8947f7f94b2c238257f0330fe
freddy24-7 <freddy.austli@aol.com>
2024-04-03 21:17:41 +0200

    adding functionalities

--- app/loading.jsx
+++ app/loading.jsx
@@ -2,19 +2,19 @@
 import ClipLoader from 'react-spinners/ClipLoader';
 
 const override = {
-    display: 'block',
-    margin: '100px auto',
+  display: 'block',
+  margin: '100px auto',
 };
 
 const LoadingPage = ({ loading }) => {
-    return (
-        <ClipLoader
-            color='#3b82f6'
-            loading={loading}
-            cssOverride={override}
-            size={150}
-            aria-label='Loading Spinner'
-        />
-    );
+  return (
+    <ClipLoader
+      color='#3b82f6'
+      loading={loading}
+      cssOverride={override}
+      size={150}
+      aria-label='Loading Spinner'
+    />
+  );
 };
 export default LoadingPage;
